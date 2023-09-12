b6d452fc9fd650118c7ebcfb5d7e7bbb159cc132
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-09-12 09:48:22 +0200

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -11,6 +11,6 @@ def inherits_from(obj, a_class):
     returns:
         True or False
     """
-    if type(obj) == a_class:
+    if type(obj).isinstance(a_class):
         return False
     return issubclass(type(obj), a_class)

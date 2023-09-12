4927a7b422c69815ce75e59bb4b94f697a4a2ba9
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-09-12 14:56:28 +0200

    update files

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -11,6 +11,26 @@ class Student:
         self.last_name = last_name
         self.age = age
 
-    def to_json(self):
-        """Get representation of student instance."""
-        return vars(self)
+    def to_json(self, attrs=None):
+        """Get representation of student.
+
+        args:
+            attrs: attributes
+        return:
+            void
+        """
+        if not attrs:
+            return vars(self)
+
+        return ({key: value for key, value in self.__dict__.items()
+                 if key in attrs})
+
+    def reload_from_json(self, json):
+        """Replace attributes.
+
+        args:
+            json: object
+        return:
+            void
+        """
+        self.__dict__.update(json)

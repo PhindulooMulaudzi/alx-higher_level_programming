6612b0dc3138cf3ced1423b826e7a2007c6c6715
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-09-12 09:59:09 +0200

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,16 +1,18 @@
-"""Rectangle subclass Square."""
+#!/usr/bin/python3
+"""Define square module."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Model a square."""
+    """Inherit from Rectangle."""
 
     def __init__(self, size):
-        """Initialize new square.
-
-        Args:
-            size (int): The size new square.
-        """
-        self.integer_validator("size", size)
-        super().__init__(size, size)
+        """Initialise method."""
+        self.integer_validator('size', size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """Print method."""
+        return "[{}] {}/{}".format(__class__.__name__,
+                                   self.__size, self.__size)

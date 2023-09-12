764bec2ff9fe57832c2a960ef50bd96e58de97f9
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-09-12 14:49:03 +0200

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,26 +1,53 @@
 #!/usr/bin/python3
-
-"""Module for stats."""
-
-import sys
+"""Print stats module."""
 
 
-status = {}
-size = 0
-count = 0
-for line in sys.stdin:
-    split = line.split()
-    status = split[-2]
-    size += int(split[-1])
-    if status in status.keys():
-        status[status] += 1
-    else:
-        status[status] = 1
-    count += 1
-    if count == 10:
-        sortme = sorted(status.keys())
-        print("File size:", size)
-        for keys in sortme:
-            print("{}: {}".format(keys, status[keys]))
-        count = 0
-        continue
+def print_stats(size, status_codes):
+    """Print metrics.
+
+    Args:
+        size (int): accumulated size.
+        status_codes (dict): accumulated count codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
+
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
+            else:
+                count += 1
+
+            line = line.split()
+
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise

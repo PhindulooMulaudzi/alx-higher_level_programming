67dec0b4649b4aa29d16dfa9f7202e3699eb9b62
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2023-11-15 12:16:41 +0200

    101 and 102 re-do

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,44 +1,6 @@
 #!/usr/bin/node
-'use strict'; // Enforce strict mode
-
 const fs = require ('fs');
 
-// Check if the correct number of command line arguments are provided
-if (process.argv.length !== 5) {
-  console.error ('Usage: ./script.js <fileA> <fileB> <outputFile>');
-  process.exit (1); // Exit with an error code
-}
-
-const fileAPath = process.argv[2];
-const fileBPath = process.argv[3];
-const outputFilePath = process.argv[4];
-
-// Read the contents of fileA
-let fileAContent;
-try {
-  fileAContent = fs.readFileSync (fileAPath, 'utf-8');
-} catch (error) {
-  console.error (`Error reading ${fileAPath}: ${error.message}`);
-  process.exit (1); // Exit with an error code
-}
-
-// Read the contents of fileB
-let fileBContent;
-try {
-  fileBContent = fs.readFileSync (fileBPath, 'utf-8');
-} catch (error) {
-  console.error (`Error reading ${fileBPath}: ${error.message}`);
-  process.exit (1); // Exit with an error code
-}
-
-// Concatenate the contents of fileA and fileB
-const concatenatedContent = fileAContent + fileBContent;
-
-// Write the concatenated content to the outputFile
-fs.writeFileSync (outputFilePath, concatenatedContent);
-
-console.log (`Contents of ${fileAPath}: "${fileAContent}"`);
-console.log (`Contents of ${fileBPath}: "${fileBContent}"`);
-console.log (
-  `Files concatenated successfully. Result written to ${outputFilePath}.`
-);
+const fArg = fs.readFileSync (process.argv[2]).toString ();
+const sArg = fs.readFileSync (process.argv[3]).toString ();
+fs.writeFileSync (process.argv[4], fArg + sArg);

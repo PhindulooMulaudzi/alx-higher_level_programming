6988615715d65b46272211f58d582636aee42e6f
Phindulo Mulaudzi <62767186+PhindulooMulaudzi@users.noreply.github.com>
2024-01-12 12:07:32 +0200

    add sort ASC

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     cursor = db.cursor()
     cursor.execute('SELECT cities.name FROM cities JOIN states ON\
     cities.state_id = states.id WHERE states.name = %s\
-    ORDER BY cities.id', (argv[4], ))
+    ORDER BY cities.id ASC', (argv[4], ))
 
     queried = cursor.fetchall()
     countula = 0

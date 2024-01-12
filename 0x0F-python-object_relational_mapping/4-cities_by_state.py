0a93e10bffdb798b52a1e2cd865552773541142e
Phindulo Mulaudzi <po.mulaudzi@gmail.com>
2024-01-12 01:42:00 -0800

    fix pep8 for 4-cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     db = MySQLdb.connect(user=argv[1], passwd=argv[2],
                          db=argv[3], port=3306, host='localhost')
     cursor = db.cursor()
-    cursor.execute('SELECT cities.id, cities.name, states.name FROM cities JOIN\
-    states ON cities.state_id = states.id;')
+    cursor.execute('SELECT cities.id, cities.name, states.name FROM cities\
+    JOIN    states ON cities.state_id = states.id;')
 
     queried = cursor.fetchall()
     for i in queried:
